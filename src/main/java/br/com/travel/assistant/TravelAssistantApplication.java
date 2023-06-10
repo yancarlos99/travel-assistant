9d2db13a833d6564b82f5938b71356df35e6dd24
Yan Carlos <yancarlos_99@outlook.com>
2023-06-10 18:22:28 -0300

    initial project creation

---
+++ src/main/java/br/com/travel/assistant/TravelAssistantApplication.java
@@ -0,0 +1,13 @@
+package br.com.travel.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/travel/assistant/TravelAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.travel.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
